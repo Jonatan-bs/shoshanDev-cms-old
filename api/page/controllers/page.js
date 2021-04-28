19d1094b66b901050c27d75ba2d52927ee6691c7
Jonatan-bs <jonatan-bs@live.dk>
2021-04-28 09:18:44 +0200

    Added findOneBySlug endpoints to pages and products

--- api/page/controllers/page.js
+++ api/page/controllers/page.js
@@ -1,8 +1,16 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findOneBySlug(ctx) {
+    const { full_slug } = ctx.params;
+
+    const entity = await strapi.services.page.findOne({ full_slug });
+    return sanitizeEntity(entity, { model: strapi.models.page });
+    },
+};
