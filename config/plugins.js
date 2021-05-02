53dfbf3380549385037c456027994d90a7fbf4e2
Jonatan-bs <jonatan-bs@live.dk>
2021-05-02 11:52:28 +0200

    Added Cloudinary Stage Folder

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,42 @@
-module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {
-            folder: env('CLOUDINARY_FOLDER'),
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === "development"){
+    return ({
+      // ...
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
         },
-        delete: {},
+        actionOptions: {
+          upload: {
+              folder: env('CLOUDINARY_FOLDER_STAGE'),
+          },
+          delete: {},
+        },
       },
-    },
-    // ...
-  });
+      // ...
+    }) } else {
+      return ({
+        // ...
+        upload: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {
+                folder: env('CLOUDINARY_FOLDER'),
+            },
+            delete: {},
+          },
+        },
+        // ...
+      })
+    }
+}
+  
