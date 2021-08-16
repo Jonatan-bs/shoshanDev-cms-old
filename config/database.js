0bd7d0941dccf7f97df1c7265f8127063fc29a05
Jonatan Shoshan <jonatan-bs@live.dk>
2021-08-16 20:37:59 +0200

    Remove stage database

--- config/database.js
+++ config/database.js
@@ -1,37 +1,17 @@
-module.exports = ({ env }) => {
- if(env('NODE_ENV') === "development"){
-  return ({
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'mongoose',
-          settings: {
-            "client": "mongo",
-            "uri": env('DATABASE_URI_STAGE'),
-          },
-          options: {
-            authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-            ssl: env.bool('DATABASE_SSL', true),
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        "client": "mongo",
+        "uri": env('DATABASE_URI'),
       },
-    });
-  } else {
-    return ({
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'mongoose',
-          settings: {
-            "client": "mongo",
-            "uri": env('DATABASE_URI'),
-          },
-          options: {
-            authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-            ssl: env.bool('DATABASE_SSL', true),
-          },
-        },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
-    });
-  }
-}
+    },
+  },
+});
+
